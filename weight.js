557ba68b28bf79f279eb76be433b46495552bb57
nicolas <nicolas.matteazzi@gmail.com>
2018-08-26 11:00:32 +0200

    Authorization header accepted

--- weight.js
+++ weight.js
@@ -8,17 +8,19 @@ var postWeightDlg = require('./dlg/PostWeightDelegate');
 var getWeightDlg = require('./dlg/GetWeightDelegate');
 var deleteWeightDlg = require('./dlg/DeleteWeightDelegate');
 
+var apiName = 'weight';
+
 var app = express();
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, GoogleIdToken");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST, DELETE");
   next();
 });
 app.use(bodyParser.json());
 
-app.get('/', function(req, res) {res.send({status: 'running'});});
+app.get('/', function(req, res) {res.send({api: apiName, status: 'running'});});
 app.get('/weights', function(req, res) {logger.apiCalled('weight', '/weights', 'GET', req.query, req.params, req.body); getWeightsDlg.getWeights().then(function(result) {res.send(result);});});
 app.post('/weights', function(req, res) {logger.apiCalled('weight', '/weights', 'POST', req.query, req.params, req.body); postWeightDlg.postWeight(req.body).then(function(result) {res.send(result);});});
 app.get('/weights/:id', function(req, res) {logger.apiCalled('weight', '/weights/{id}', 'GET', req.query, req.params, req.body); getWeightDlg.getWeight(req.params.id).then(function(result) {res.send(result);});});
