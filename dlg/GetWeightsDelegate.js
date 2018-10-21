843c0d3fbdbc6635230ac9948fa102bb2c1c9629
nicolas <nicolas.matteazzi@gmail.com>
2018-10-21 10:09:27 +0200

    Filtering possibility on the GET /weights (filter per dateFrom)

--- dlg/GetWeightsDelegate.js
+++ dlg/GetWeightsDelegate.js
@@ -1,16 +1,33 @@
 var mongo = require('mongodb');
 var config = require('../config');
 var converter = require('../conv/WeightConverter');
+var moment = require(moment-timezone);
 
 var MongoClient = mongo.MongoClient;
 
-exports.getWeights = function() {
+exports.getWeights = function(dateFrom) {
 
   return new Promise(function(success, failure) {
 
     return MongoClient.connect(config.mongoUrl, function(err, db) {
 
-      var results = db.db(config.dbName).collection(config.collections.weights).find().sort(converter.weightConverter.sortDateDesc());
+      // Define an empty filter
+      let filter = {};
+
+      // IF date from is passed, use it as a filter
+      if (dateFrom != null) {
+        // Extract the week out of the date
+        let d = moment(dateFrom, 'YYYYMMDD').tz(config.timezone);
+        let week = d.format('WW');
+        let year = d.format('YYYY');
+
+        // Update the filter
+        filter = {week: week, year: year};
+      }
+
+      var results = db.db(config.dbName).collection(config.collections.weights)
+          .find(filter)
+          .sort(converter.weightConverter.sortDateDesc());
 
       results.toArray(function(err, array) {
         db.close();
